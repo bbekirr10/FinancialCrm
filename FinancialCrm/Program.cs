35361a0a81f06ee49174cb661ef9fecb37ed462f
bbekirr10 <bbekirr_yavuz@hotmail.com>
2025-01-19 01:07:13 +0300

    FinancialCrm Proje

--- FinancialCrm/Program.cs
+++ FinancialCrm/Program.cs
@@ -17,7 +17,7 @@ namespace FinancialCrm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmBanks());
+            Application.Run(new FrmUserLogin());
         }
     }
 }
